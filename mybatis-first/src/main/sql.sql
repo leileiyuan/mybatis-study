0e40583bc24bf510151b80c11f94a0e272446844
yuanleilei628 <yuanleilei628@163.com>
2018-03-24 09:01:31 +0800

    mybatis first

---
+++ mybatis-first/src/main/sql.sql
@@ -0,0 +1,11 @@
+-- auto-generated definition
+CREATE TABLE t_person
+(
+  id          INT AUTO_INCREMENT
+    PRIMARY KEY,
+  username    VARCHAR(255) NULL,
+  age         INT          NULL,
+  mobilephone VARCHAR(255) NULL
+)
+  ENGINE = InnoDB;
+
---
+++ mybatis-first/src/test/java/com/yuanleilei/mybatis/dao/UserTest.java
@@ -0,0 +1,51 @@
+package com.yuanleilei.mybatis.dao;
+
+import com.yuanleilei.mybatis.model.Person;
+import com.yuanleilei.mybatis.utils.MybatisUtil;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+public class UserTest {
+
+    SqlSession sqlSession;
+
+    @Test
+    public void insertPerson() {
+        sqlSession = MybatisUtil.getSqlSession();
+        int id = 10000;
+        String userName = "test";
+        int age = 18;
+        String mobilePhone = "18000000000";
+        Person person = new Person();
+        person.setId(id);
+        person.setAge(age);
+        person.setUserName(userName);
+        person.setMobilePhone(mobilePhone);
+        try {
+            sqlSession.insert("insertPerson", person);
+            sqlSession.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            MybatisUtil.closeSession(sqlSession);
+        }
+
+    }
+
+    @Test
+    public void queryById() {
+        sqlSession = MybatisUtil.getSqlSession();
+        int id = 1;
+        try {
+            Person person = sqlSession.selectOne("queryById", id);
+
+            sqlSession.commit();
+            System.out.println(person.getUserName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            MybatisUtil.closeSession(sqlSession);
+        }
+
+    }
+}
